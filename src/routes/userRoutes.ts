95ccb8fdd488b3924b68e1705d2b9bc5d92be7b6
fuchsgerson <fuchsgerson@gmail.com>
2025-01-13 10:32:55 -0300

    feat(DELETE user)

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
   createUser,
+  deleteUser,
   getUsers,
   updateUser,
 } from "src/controllers/userController";
+import { adminOnlyMiddleware } from "src/middlewares/adminOnlyMiddleware";
 import { authMiddleware } from "src/middlewares/authMiddleware";
 import { roleValidationMiddleware } from "src/middlewares/roleValidationMiddleware";
 
@@ -12,5 +14,6 @@ const router = Router();
 router.get("/", authMiddleware, getUsers);
 router.post("/", authMiddleware, roleValidationMiddleware, createUser);
 router.put("/:id", authMiddleware, roleValidationMiddleware, updateUser);
+router.delete("/:id", authMiddleware, adminOnlyMiddleware, deleteUser);
 
 export default router;
