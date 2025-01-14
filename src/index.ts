3532d2fe01483b5e4f1458a35bdfa4ca34a6f0cc
fuchsgerson <fuchsgerson@gmail.com>
2025-01-14 19:40:15 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+const server = app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;  
 //  "email": "admin2@example.com",
 // "password": "password123",
